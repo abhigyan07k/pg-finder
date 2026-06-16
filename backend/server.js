e7a6336d84b1d66de65eea2cf0373f141543a8fb
Abhigyan Kumar <abhigyan07k@gmail.com>
2026-06-16 11:42:37 +0530

    fix cors

--- backend/server.js
+++ backend/server.js
@@ -23,10 +23,18 @@ const allowedOrigins = [
   process.env.FRONTEND_URL,
 ];
 
-app.use(cors({
-  origin: process.env.FRONTEND_URL,
-  credentials: true
-}));
+app.use(
+  cors({
+    origin: function (origin, callback) {
+      if (!origin || allowedOrigins.includes(origin)) {
+        callback(null, true);
+      } else {
+        callback(new Error("Not allowed by CORS"));
+      }
+    },
+    credentials: true,
+  })
+);
 
 app.use(express.json());
 app.use("/uploads", express.static("uploads"));
